97da44014cce8ea97cea5b38c77f5ffdf67970c6
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-02 23:35:07 -0200

    Configuração inicial no arquivo 'webpack.config.js' para iniciar os exer. de React.

---
+++ exercicios_react/webpack.config.js
@@ -0,0 +1,30 @@
+/**
+ * Arquivo: webpack.config.js
+ * Data: 02/11/2017
+ * Descrição: Arquivo responsável por realizar a transpilação dos arquivos do projeto.
+ * Author: Glaucia Lemos
+ */
+
+const webpack = require('webpack')
+
+module.exports = {
+    entry: './ex/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+}
