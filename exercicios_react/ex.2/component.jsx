51b1a1b06cd0a78ca93c174470fc83bacddaec14
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-03 00:02:05 -0200

    Alterando o component com as convenções do ES2015

--- exercicios_react/ex.2/component.jsx
+++ exercicios_react/ex.2/component.jsx
@@ -7,7 +7,6 @@
 
 import React from 'react'
 
-export default () => (
-    <h1>Primeiro Componente!!!</h1>
-)
-
+export default function() {
+    return <h1>Primeiro Componente!!!</h1>
+}
---
+++ exercicios_react/ex.2/index.jsx
@@ -0,0 +1,12 @@
+/**
+ * Arquivo: index.jsx
+ * Data: 02/11/2017
+ * Descrição: arquivo porta de entrada e responsável pela lógica a ser executada via Js.
+ * Author: Glaucia Lemos
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Component from './component'
+
+ReactDOM.render(<Component />, document.getElementById('app'))
