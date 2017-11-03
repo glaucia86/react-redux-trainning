d953a874128e3437da3c55f422a472a842eccb90
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-02 23:47:10 -0200

    Testando o arquivo 'index.jsx'

---
+++ exercicios_react/ex/index.jsx
@@ -0,0 +1,14 @@
+/**
+ * Arquivo: index.jsx
+ * Data: 02/11/2017
+ * Descrição: arquivo porta de entrada e responsável pela lógica a ser executada via Js.
+ * Author: Glaucia Lemos
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    <h1>Olá Glaucia Lemos!!!</h1>,
+    document.getElementById('app')
+)
