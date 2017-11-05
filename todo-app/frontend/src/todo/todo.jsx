30fe09744bb6163296b88d10a485019ae64dac62
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-05 21:32:57 -0200

    Desenvolvimento dos componentes : About & Film

---
+++ todo-app/frontend/src/todo/todo.jsx
@@ -0,0 +1,18 @@
+/**
+ * Arquivo: src/film.jsx
+ * Data: 05/11/2017
+ * Descrição: Arquivo responsável pelos Componentes relacionado a Todo.
+ * Author: Glaucia Lemos
+ */
+
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
