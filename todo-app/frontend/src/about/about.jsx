9defe095f6f76468ccbce3cb1014a3da4c49b685
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-11-05 22:23:54 -0200

    Desenvolvimento do PageHeader C omponent

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,14 +1,46 @@
-/**
- * Arquivo: src/about.jsx
- * Data: 05/11/2017
- * Descrição: Arquivo responsável pelos Componentes relacionado a About.
- * Author: Glaucia Lemos
- */
-
-import React from 'react'
+import React from "react";
+import PageHeader from "../template/pageHeader";
 
 export default props => (
-    <div>
-        <h1>Sobre: Glaucia Lemos</h1>
-    </div>
+  <div>
+    <PageHeader name="Sobre" small="Nós" />
+    <h2>Glaucia Lemos</h2>
+    <p>Software Engineer & Microsoft MVP apaixonada pelo que faz!</p>
+
+    <h2>Contribuições Técnicas/Links Importantes:</h2>
+    <ul>
+      <li>
+        Site Técnico: <a href="www.code4coders.wordpress.com">Code4Coders</a>
+      </li>
+      <li>
+        Youtube:<a href="https://www.youtube.com/user/l32759">
+          Canal Youtube - Glaucia Lemos
+        </a>
+      </li>
+      <li>
+        GitHub: <a href="https://github.com/glaucia86/">Repositório GitHub</a>
+      </li>
+      <li>
+        Curso TypeScript - Udemy:<a href="https://www.udemy.com/curso-typescript-para-desenvolvedores-c">
+          Curso TypeScript - Glaucia Lemos
+        </a>
+      </li>
+    </ul>
+
+    <h2>Informações sobre a App Desenvolvida:</h2>
+    <ul>
+      <li>Node.Js</li>
+      <li>React</li>
+      <li>Redux</li>
+      <li>Express.Js</li>
+      <li>MongoDb</li>
+      <li>Mongoose</li>
+      <li>Visual Studio Code</li>
+    </ul>
+    <p>
+      Mais Informações:<a href="https://github.com/glaucia86/desafio-gupy-glaucia">
+        AQUI
+      </a>
+    </p>
+  </div>
 )
--- todo-app/frontend/src/todo/todo.jsx
+++ todo-app/frontend/src/todo/todo.jsx
@@ -6,12 +6,13 @@
  */
 
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default class Todo extends Component {
     render() {
         return(
             <div>
-                <h1>Todo</h1>
+                <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
             </div>
         )
     }
